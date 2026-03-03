809d8bf0a51e6d5db3bf5c6bcc7575ed207ddfed
Brian Ochieng <bochieng228@gmail.com>
2026-03-03 13:45:15 +0300

    text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
             <section class="bg-[url('../assets/img.jpg')] bg-center bg-cover">
                 <Nav/>
                 <div class="flex items-center justify-center flex-col text-center">
-                    <h1 class="text-4xl md:text-8xl lg:text-16xl font-bold">Mr Man Fitness</h1>
+                    <h1 class="text-4xl md:text-8xl lg:text-16xl font-bold text-blue-700">Mr Man Fitness</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium minima optio, 
                         ab ipsa ullam iste ad quisquam culpa est aut 
                         corporis numquam. Consectetur distinctio, earum laudantium repellendus animi atque 
