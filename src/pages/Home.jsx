7c819797f81eb6b2a4137e2c552eb2cbde397975
Brian Ochieng <bochieng228@gmail.com>
2026-03-03 16:18:11 +0300

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import Nav from '../components/Nav'
 import heroImg from "../assets/img.jpg";
+import profile from "../assets/geff.jpeg";
 export default function Home(){
     return(
         <>
@@ -23,6 +24,29 @@ export default function Home(){
                         <img src={heroImg} alt="Hero Image" className='shadow-lg rounded-2xl '/>
                     </div>
                 </div>
+            </section>
+            <section className="px-6">
+                <h2 className="text-blue-500 text-2xl md:text-4xl lg:text-6xl text-semibold text-center">
+                    About <span >Mr Man</span></h2>
+
+                    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 items-center justify-center pt-4 pb-4">
+                        <div>
+                            <p class="py-6 text-gray-500 text-center md:text-left lg:text-left">
+                                <span class="text-red-500 text-bold">Mr Man Fitness </span> is about disciplined training and purposeful living. It focuses on 
+                                building real strength, an aesthetic physique, and mental toughness through consistent 
+                                workouts, structured routines, and a growth-driven mindset. The goal is not quick results 
+                                but lasting transformation — developing the habits, discipline, and resilience that shape 
+                                a strong man over time.
+                                At Mr Man Fitness, progress comes from commitment, effort, and daily improvement. Members are encouraged to push their 
+                                limits, stay focused on their goals, and build confidence through physical and mental development. It’s a space where dedication is valued and growth is expected.
+                                It’s not just about muscles — it’s about becoming a stronger, more confident, and disciplined version of yourself every 
+                                single day.
+                            </p>
+                        </div>
+                        <div>
+                            <img src={profile} alt="fitness profile" className=''/>
+                        </div>
+                    </div>
             </section>
         </main>
         </>
