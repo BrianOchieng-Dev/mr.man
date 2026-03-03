0e94702c2d349617951f8be7b2e79a0d8a83bc2a
Brian Ochieng <bochieng228@gmail.com>
2026-03-03 15:19:54 +0300

    text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,13 @@ export default function Home(){
                      <h2 className="text-gray-500 lg:px-12 md:px-6 px-2">
                         <span class="text-xl md:text-2xl lg:text-2xl text-green-500 p-2 border-2 rounded-2xl m-2">Built Strong..</span>  
                         <span class="text-xl md:text-2xl lg:text-2xl text-green-500 p-2 border-2 rounded-2xl m-2">Living Sharp</span></h2>
-                    <h1 className="text-4xl md:text-8xl lg:text-12xl font-semibold text-blue-500">Mr Man Fitness</h1>
+                    <h1 className="text-4xl md:text-8xl lg:text-12xl font-semibold text-blue-500 p-2">Mr Man Fitness</h1>
                    
 
                     <div>
                         <button className="m-2 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">Book a Session</button>
                     </div>
-                    <div className='m-12'>
+                    <div className='m-2 md:m-4 lg:m-8'>
                         <img src={heroImg} alt="Hero Image" className='shadow-lg rounded-2xl '/>
                     </div>
                 </div>
